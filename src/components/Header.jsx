c69cf0ca0e9249a6cfc49b1f204bf3174795fee6
emperorlinc <olowoyojames38@gmail.com>
2025-10-20 12:52:46 +0100

    Created a header component and added it to the landing page.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,41 @@
 import { CiMenuBurger } from "react-icons/ci";
 import { BiSearch, BiShoppingBag } from "react-icons/bi";
 
-const Header = () => {
+const Header = ({ toggleCart }) => {
   return (
-    <div className="header">
-      <button className="remove-outline-btn">
-        <CiMenuBurger />
-      </button>
-      <div className="search-wrapper">
-        <BiSearch />
-        <input type="text" placeholder="What are you looking for?" />
+    <nav className="navbar">
+      <div className="nav-container">
+        <div className="nav-logo">
+          <i data-feather="cake" className="logo-icon"></i>
+          <span className="logo-text">Kaiju's Cake</span>
+        </div>
+        <div className="nav-menu">
+          <a href="#" className="nav-link active">
+            Home
+          </a>
+          <a href="#" className="nav-link">
+            Cakes
+          </a>
+          <a href="#" className="nav-link">
+            About
+          </a>
+          <a href="#" className="nav-link">
+            Contact
+          </a>
+        </div>
+        <div className="nav-actions">
+          <button id="cart-toggle" className="cart-button" onClick={toggleCart}>
+            <BiShoppingBag />
+            <span id="cart-count" className="cart-count">
+              0
+            </span>
+          </button>
+          <button className="mobile-menu-button">
+            <CiMenuBurger />
+          </button>
+        </div>
       </div>
-      <button className="remove-outline-btn">
-        <BiShoppingBag className="shopping-bag-icon" />
-      </button>
-    </div>
+    </nav>
   );
 };
 

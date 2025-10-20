3718fc26a4502529cdc8668ff59b0478fda7c18f
emperorlinc <olowoyojames38@gmail.com>
2025-10-20 12:51:17 +0100

    Create and added the category component to the landing page.

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,9 +1,16 @@
-const Category = ({ category }) => {
+const Category = ({ category, productCategory, getProducts }) => {
   return (
     <div className="category-wrapper">
       <div className="category-items">
+        <div className="category" onClick={() => getProducts()}>
+          All
+        </div>
         {category.map((element) => (
-          <div key={element.id} className="category">
+          <div
+            key={element.id}
+            className="category"
+            onClick={() => productCategory(element.id)}
+          >
             {element.name}
           </div>
         ))}

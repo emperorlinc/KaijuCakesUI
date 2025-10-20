1f8cec80e144f468f31f0628a79c4443fdd73ae7
emperorlinc <olowoyojames38@gmail.com>
2025-10-20 12:49:46 +0100

    Added a carousel component to the landing page.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,16 @@
 const Carousel = () => {
-  return <div className="carousel-wrapper">Carousel</div>;
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        {/* <h3>Carousel</h3> */}
+        <div className="hero-cta">
+          <a href="#" className="cta-button">
+            Explore Our Cakes
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Carousel;

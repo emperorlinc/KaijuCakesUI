819591316216d40183cc0753bd96511a3ca09fde
emperorlinc <olowoyojames38@gmail.com>
2025-10-20 13:02:16 +0100

    Created the landing page of the webpage that holds other components like header, category and products.

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,14 +2,26 @@ import Header from "../components/Header";
 import Category from "../components/Category";
 import Products from "../components/Products";
 import Carousel from "../components/Carousel";
+import CartSidebar from "../components/CartSidebar";
 
-const LandingPage = ({ category, product }) => {
+const LandingPage = ({
+  category,
+  product,
+  toggleCart,
+  productCategory,
+  getProducts,
+}) => {
   return (
     <div className="container">
-      <Header />
+      <Header toggleCart={toggleCart} />
       <Carousel />
-      <Category category={category} />
+      <Category
+        category={category}
+        productCategory={productCategory}
+        getProducts={getProducts}
+      />
       <Products product={product} />
+      <CartSidebar toggleCart={toggleCart} />
     </div>
   );
 };
